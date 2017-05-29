b848de91bd70925194a347c5aedcdb31c989e65a
vovnet <vovnet@yandex.ru>
2017-05-29 14:26:41 +0300

    add pretty links, .htaccess files

--- config/web.php
+++ config/web.php
@@ -10,6 +10,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'V-pGiKJ5o7u74UHTulzQNcFfKyjXL76H',
+            'baseUrl' => ''
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -38,14 +39,14 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
+
     ],
     'params' => $params,
 ];
